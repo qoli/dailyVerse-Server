ddc670aec1f6345dbdb41f4a05921787b43958b5
Qoli Wong <llqoli@gmail.com>
2018-01-15 17:35:47 +0800

    fix

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-
-$curl = curl_init("https://www.taiwanbible.com/blog/dailyverse.jsp");
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl,CURLOPT_POSTFIELDS,$_POST);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-
-$data = curl_exec($curl);
-
-echo $data;
+$home = file_get_contents('https://www.taiwanbible.com/blog/dailyverse.jsp');
+$result = mb_convert_encoding($home,"utf-8","big5");
+echo $result;
