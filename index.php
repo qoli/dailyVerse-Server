c69c0fcb1cf678a5104e896b3b5dbb1cfdc85b9c
Qoli Wong <llqoli@gmail.com>
2018-01-15 15:48:57 +0800

    fix

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-$application_server = 'www.taiwanbible.com'; // replace with IP or hostname of application server
-$uri = $_SERVER['REQUEST_URI']; // you may need to change this, not sure from your question.
-
-$curl = curl_init("https://{$application_server}{$uri}");
+$curl = curl_init("https://www.taiwanbible.com/blog/dailyverse.jsp");
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl,CURLOPT_POSTFIELDS,$_POST);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
