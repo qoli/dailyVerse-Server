be6c7ff5187deccbd25b01676c5d9085b1bb646f
Qoli Wong <llqoli@gmail.com>
2019-01-27 14:10:10 +0800

    追加朗讀 API

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
+// 彼得前書 1 : 22 你們既因順從真理，潔淨了自己的心，以致愛弟兄沒有虛假，就當從心裡（從心裡：有古卷是從清潔的心）彼此切實相愛。
+
 header('Content-Type: text/html; charset=utf-8');
-$home = file_get_contents('https://www.taiwanbible.com/blog/dailyverse.jsp');
-// $home = mb_convert_encoding($home,"utf-8","big5");
-echo $home;
+$file = file_get_contents('https://www.taiwanbible.com/blog/dailyverse.jsp');
+echo $file;
---
+++ voice.php
@@ -0,0 +1,57 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: application/json');
+require_once 'MainFunction.php';
+
+// https://bible.fhl.net/new/read.php
+
+$url = $_GET['link'] . "?chap=" . $_GET["chap"] . "&chineses=" . $_GET["chineses"];
+$ver = 'unv1';
+
+$result  = file_get_contents($url);
+$pattern = '~<a href="listenhb.php\?version=(\d?)(.*?)">(.*?)</a>~i';
+preg_match_all($pattern, $result, $matches);
+parse_str(parse_url("listenhb.php?version=0" . $matches[2][0], PHP_URL_QUERY), $output);
+
+foreach ($matches[1] as $key => $value) {
+	$verNumber[] = 'http://media.fhl.net/' . getVersion($value) . '/' . $output['amp;bid'] . '/' . $output['amp;bid'] . '_' . $output['amp;chap'] . '.mp3';
+}
+
+$return = array(
+	"url" => 'http://media.fhl.net/' . getVersion(0) . '/' . $output['amp;bid'] . '/' . $output['amp;bid'] . '_' . $output['amp;chap'] . '.mp3',
+	"count" => count($matches[2]),
+    "versionName" => $matches[3],
+    "audioURL" => $verNumber,
+);
+
+// dump($return);
+
+echo json_encode($return);
+
+function getVersion($verNumber = 0)
+{
+    switch ($verNumber) {
+        case 4:
+            return 'tcv';
+            break;
+        case 1:
+            return 'Taiwanese';
+            break;
+        case 5:
+            return 'tai';
+            break;
+        case 3:
+            return 'Cantonese1';
+            break;
+        case 8:
+            return 'fj';
+            break;
+        case 9:
+            return 'greek';
+            break;
+
+        default:
+            return 'unv1';
+            break;
+    }
+}
