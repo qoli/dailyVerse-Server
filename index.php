48fdd8e3f0d0d37dfe10b4a26f75973878dd602f
Qoli Wong <llqoli@gmail.com>
2019-07-09 12:47:03 +0800

    修正遠端 API 返回空的問題

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 require_once 'MainFunction.php';
-// 彼得前書 1 : 22 你們既因順從真理，潔淨了自己的心，以致愛弟兄沒有虛假，就當從心裡（從心裡：有古卷是從清潔的心）彼此切實相愛。
 
-$now       = getdate();
+$now = getdate();
 $dayInFile = file_get_contents("dayInFile.txt");
 
 if ($dayInFile == $now["mday"]) {
-    $dailyVerse = file_get_contents("dailyVerse.txt");
-    echo $dailyVerse;
+	$dailyVerse = file_get_contents("dailyVerse.txt");
+	echo $dailyVerse;
 } else {
-    file_put_contents("dayInFile.txt", $now["mday"]);
-    $verse = file_get_contents('https://www.taiwanbible.com/blog/dailyverse.jsp');
-    file_put_contents("dailyVerse.txt", trim($verse));
-    $dailyVerse = file_get_contents("dailyVerse.txt");
-    echo $dailyVerse;
+	$verse = file_get_contents('https://www.taiwanbible.com/blog/dailyverse.jsp');
+	if (trim($verse) != "") {
+		file_put_contents("dayInFile.txt", $now["mday"]);
+		file_put_contents("dailyVerse.txt", trim($verse));
+		$dailyVerse = file_get_contents("dailyVerse.txt");
+		echo $dailyVerse;
+	} else {
+		echo trim($verse);
+	}
 }
